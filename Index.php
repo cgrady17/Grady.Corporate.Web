ae3c6dbf93780ccbe7ed2182291599cf2c26e67b
Connor Grady <connor.grady@gradycapital.us>
2015-02-11 16:18:14 -0600

    Add content shell to Index.php

--- Index.php
+++ Index.php
@@ -2,6 +2,10 @@
 <?php require_once'Includes/Header.php'; ?>
 
 <!-- Page HTML Here -->
+    <div id="grady-wrapper">
 
+        <div class="page-content">
+            <h1>Content coming soon!</h1>
+        </div>
 
 <?php require_once'Includes/Footer.php'; ?>
